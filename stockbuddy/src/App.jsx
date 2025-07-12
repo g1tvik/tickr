8b96361b2fed6f51078b400aa48a2be0325089b1
ritvik <ritvikvudatha@gmail.com>
2025-07-11 17:16:01 -0700

    insane sidebar custom made BY RITVIK

--- stockbuddy/src/App.jsx
+++ stockbuddy/src/App.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home"; // or "./pages/Home.tsx"
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/Signup";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
-
-  // Pass setIsLoggedIn to SignIn/SignOut components so they can update login state
   return (
     <Router>
-      <NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
