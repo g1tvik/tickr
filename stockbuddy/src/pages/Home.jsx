0c34b271750a3a25751e82bd92fcf6edf9059b85
ritvik <ritvikvudatha@gmail.com>
2025-07-13 16:00:45 -0700

    set up Alpha Vantage API, set up top gainers and losers, cleaned up ticker look

--- stockbuddy/src/pages/Home.jsx
+++ stockbuddy/src/pages/Home.jsx
@@ -31,7 +31,7 @@ function Home({ isLoggedIn }) {
         <h1 className="fw-bold text-center mb-3 hero-title">
           <Typewriter 
             text="Invest smarter. Learn faster." 
-            speed={100}
+            speed={60}
             className="fw-bold"
           />
         </h1>
